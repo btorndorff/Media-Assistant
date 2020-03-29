e56f882c7133fd2014c821d6414b6b967c5efe09
Ben Orndorff <bto5fs@virginia.edu>
2020-03-28 21:35:12 -0400

    Mostly working

--- MA.js
+++ MA.js
@@ -1,10 +1,15 @@
 //import dependencies 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const malScraper = require('mal-scraper')
 const {
    prefix,
    token,
 } = require('./config.json');
+
+//database
+const Enmap = require("enmap");
+client.media = new Enmap({name: "media"});
 
 //setup array for watchlist and watching (need to convert to somethings so it becomes permanent)
 var watchlist = [];
@@ -24,6 +29,13 @@ client.on('message', (message) => {
    const args = message.content.slice(prefix.length).split(' ');
    const command = args.shift().toLowerCase(); 
 
+   const key = `${message.author.id}`;
+   client.media.ensure(key, {
+     user: message.author.id,
+     watchlist: [],
+     watching: []
+   });
+
    //help command
    if (command === 'help') {
       help(message);
@@ -31,33 +43,39 @@ client.on('message', (message) => {
 
    //watchlist command
    if (command === 'wl') {
+      watchlist = client.media.get(key, "watchlist");
       if (args[0] != null) {
-         if (args[0] == "add") {
-            addWatchlist(message, args);
-         } else if (args[0] == "remove") {
-            removeWatchlist(message, args);
+         if (args[0] === "add") {
+            addWatchlist(message, args, key);
+         } else if (args[0] === "remove") {
+            removeWatchlist(message, args, key);
+         } else if (args[0] === "clear") {
+            watchlist = [];
+            message.channel.send("Cleared!");
+            client.media.set(key, watchlist, "watchlist")
          } else {
             message.channel.send("Invalid Command");
          }
       }
-      message.channel.send(listArr(message, watchlist));
+      listArr(message, watchlist, "WatchList");
    }
 
    //watching command
    if (command === 'w') {
+      watching = client.media.get(key, "watching");
       if (args[0] != null) { 
          if (args[0] == "add") {
-            //addWatching();
+            addWatching(message, args, key);
          } else if (args[0] == "remove") {
-            //removeWatching();
+            removeWatching(message, args, key);
          } else if (args[0] == "update") {
             //updateWatching();
          } else {
             message.channel.send("Invalid Command")
          }
       }
-      //listArr(message, watching);
-   } 
+      listArr(message, watching, "Watching");
+   }
 });
 
 //help function to display commands
@@ -66,7 +84,8 @@ function help(message) {
 };
 
 //adds specified title to users watchlist
-function addWatchlist(message, args) {
+function addWatchlist(message, args, key) {
+
    if (args[1] != null) {
       //get the title 
       let title = "";
@@ -75,13 +94,30 @@ function addWatchlist(message, args) {
       }
       //adds title to watchlist
       addTitle(message, title.toLowerCase(), watchlist);
+      client.media.set(key, watchlist, "watchlist");
+   } else {
+      message.channel.send("No title specified");
+   }
+}
+
+function addWatching(message, args, key) {
+   if (args[1] != null) {
+      //get the title 
+      let title = "";
+      for (let i = 1; i < args.length ; i++) {
+         title += args[i] + " ";
+      }
+      //adds title to watchlist
+      addTitle(message, title.toLowerCase(), watching);
+      removeTitle(message, title.toLowerCase(), watchlist)
+      client.media.set(key, watching, "watching");
    } else {
       message.channel.send("No title specified");
    }
 }
 
 //removes specified title from users watchlist
-function removeWatchlist(message, args) {
+function removeWatchlist(message, args, key) {
    if (args[1] != null) {
       //get the title 
       let title = "";
@@ -90,6 +126,22 @@ function removeWatchlist(message, args) {
       }
       //removes title from arr
       removeTitle(message, title.toLowerCase(), watchlist);
+      client.media.set(key, watchlist, "watchlist");
+   } else {
+      message.channel.send("No title specified");
+   }
+}
+
+function removeWatching(message, args, key) {
+   if (args[1] != null) {
+      //get the title 
+      let title = "";
+      for (let i = 1; i < args.length ; i++) {
+         title += args[i] + " ";
+      }
+      //removes title from arr
+      removeTitle(message, title.toLowerCase(), watching);
+      client.media.set(key, watching, "watching");
    } else {
       message.channel.send("No title specified");
    }
@@ -99,9 +151,8 @@ function removeWatchlist(message, args) {
 function addTitle(message, title, arr) {
    if(arr.indexOf(title) === -1) {
       arr.push(title)
-      message.channel.send("Added " + title);
    } else {
-      message.channel.send(title + " was not found in your watchlist");
+      message.channel.send(title + " is already in your watchlist");
    }
 }
 
@@ -109,19 +160,20 @@ function addTitle(message, title, arr) {
 function removeTitle(message, title, arr) {
    if(arr.indexOf(title) != -1) {
       arr.splice(arr.indexOf(title),1);
-      message.channel.send("Removed " + title);
-   } else {
-      message.channel.send(title + " was not found in your watchlist");
    }
 }
 
 //prints the watchlist
-function listArr(message, arr) {
-   let print = message.author.tag + " Watchlist: \n";
+function listArr(message, arr, arrName) {
+   let print = message.author.tag + " " + arrName + ": \n";
+   message.channel.send(print);
    for (let i = 0; i < arr.length; i++) {
-      print += "- " + arr[i] + "\n";
+      malScraper.getInfoFromName(arr[i])
+      .then((data) =>{
+         message.channel.send(data.englishTitle + " - " + data.url);
+      }
+      )
    }
-   return print;
 }
 
 client.login(token);
